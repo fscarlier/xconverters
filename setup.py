f8cc54ac1059b3f609a2422747dea2a51e6c1ca1
fscarlier <fscarlier@users.noreply.github.com>
2022-06-27 16:46:35 +0200

    http

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ DEPENDENCIES = [
     "accelerator-toolbox",
     "xtrack@https://github.com/xsuite/xtrack/tarball/main",
     "xdeps@https://github.com/xsuite/xdeps/tarball/main",
-    "xsequence @ git+ssh://git@github.com/fscarlier/xsequence.git",
+    "xsequence @ git+http://git@github.com/fscarlier/xsequence.git",
     "lark",
     "rich",
     ]
